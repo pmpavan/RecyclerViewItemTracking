1ceaae5c6a89c5ee70311f2277bd7429a52319d7
Pavan <pavanpm@freshdesk.com>
2018-07-15 12:47:38 +0530

    init

---
+++ app/src/main/java/com/pmpavan/recyyclerviewitemtracking/TrackingActivity.kt
@@ -0,0 +1,12 @@
+package com.pmpavan.recyyclerviewitemtracking
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class TrackingActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tracking)
+    }
+}
